636359f48c33f23f4804eaffb2f42a999fa3b860
José García <jose.garcia26@utp.ac.pa>
2024-05-31 19:57:25 -0500

    agregando usuarios, comentarios

--- models/detalles.js
+++ models/detalles.js
@@ -6,17 +6,8 @@ const eventos = [
       "Experimenta una competencia con otros desarrolladores y pon a prueba tus habilidades",
     fecha: "01/06/2024",
     ubicacion: "Ciudad del saber",
-    usuariosIdRegistrados: ["idanielmatt", "gilberto_m", "rodolfo_biker"],
-    comentarios: {
-      1: {
-        idUsuario: "idanielmatt",
-        comentario: "el mejor evento que experimenté.",
-      },
-      2: {
-        idUsuario: "gilberto_m",
-        comentario: "el mejor evento que experimenté.",
-      },
-    },
+    usuariosIdRegistrados: ["idanielmatt", "rodolfo_biker"],
+    idComentario: [1]
   },
   {
     idEvento: 2,
@@ -26,16 +17,7 @@ const eventos = [
     fecha: "01/10/2024",
     ubicacion: "Ciudad del saber",
     usuariosIdRegistrados: ["idanielmatt", "gilberto_m", "rodolfo_biker"],
-    comentarios: {
-      1: {
-        idUsuario: "rodolfo_biker",
-        comentario: "sé algo de linux, se ve interesante.",
-      },
-      2: {
-        idUsuario: "gilberto_m",
-        comentario: "desbarataré esa competencia papa 😎.",
-      },
-    },
+    idComentario: [2, 3]
   },
   {
     idEvento: 3,
@@ -43,22 +25,54 @@ const eventos = [
     descripcion: "Nuevas tendencias, Azure, aprende y compite en la Nube ☁️",
     fecha: "01/06/2025",
     ubicacion: "Universidad Tecnológica de Panamá",
-    usuariosIdRegistrados: ["gilberto_m", "idanielmatt", "rodolfo_biker"],
-    comentarios: {
-      1: {
-        idUsuario: "rodolfo_biker",
-        comentario: "voy pa'esa.",
-      },
-      2: {
-        idUsuario: "gilberto_m",
-        comentario: "No habia escuchado de azure antes, me apunto.",
-      },
-      3: {
-        idUsuario: "idanielmatt",
-        comentario: "Mi primera hackathon en la nube, no me la pierdo.",
-      },
-    },
+    usuariosIdRegistrados: ["gilberto_m", "rodolfo_biker"],
+    idComentario: [4]
   },
 ];
 
-module.exports = { eventos };
+const usuarios = [
+  {
+    id: "idanielmatt",
+    nombre: "José García",
+    email: "jose.garcia@gmail.com"
+  },
+  {
+    id: "gilberto_m",
+    nombre: "Gilberto Molinar",
+    email: "gilber.molinar@gmail.com"
+  },
+  {
+    id: "rodolfo_biker",
+    nombre: "Rodolfo Abrego",
+    email: "rodolfo.abrego@gmail.com"
+  }
+]
+
+const comentarios = [
+  {
+    idComentario: 1,
+    idEvento: 1,
+    idUsuario: "idanielmatt",
+    comentario: "Que gran evento."
+  },
+  {
+    idComentario: 2,
+    idEvento: 2,
+    idUsuario: "rodolfo_biker",
+    comentario: "sé algo de linux, se ve interesante.",
+  },
+  {
+    idComentario: 3,
+    idEvento: 2,
+    idUsuario: "gilberto_m",
+    comentario: "desbarataré esa competencia papa 😎.",
+  },
+  {
+    idComentario: 4,
+    idEvento: 3,
+    idUsuario: "rodolfo_biker",
+    comentario: "voy pa' esa.",
+  }
+]
+
+module.exports = { eventos, usuarios, comentarios };
